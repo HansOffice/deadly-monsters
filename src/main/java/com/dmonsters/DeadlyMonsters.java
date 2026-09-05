365ce20264b43d6977cbaec9bc5ce32e78baf2ab
HansOffice <94523696+HansOffice@users.noreply.github.com>
2026-09-06 00:21:59 +0800

    Register entity types and attributes

--- src/main/java/com/dmonsters/DeadlyMonsters.java
+++ src/main/java/com/dmonsters/DeadlyMonsters.java
@@ -2,6 +2,7 @@ package com.dmonsters;
 
 import com.dmonsters.registry.ModBlocks;
 import com.dmonsters.registry.ModCreativeTabs;
+import com.dmonsters.registry.ModEntities;
 import com.dmonsters.registry.ModItems;
 import com.mojang.logging.LogUtils;
 import net.neoforged.bus.api.IEventBus;
@@ -16,8 +17,11 @@ public final class DeadlyMonsters {
 
     public DeadlyMonsters(IEventBus modEventBus, ModContainer modContainer) {
         ModBlocks.BLOCKS.register(modEventBus);
+        ModEntities.ENTITY_TYPES.register(modEventBus);
         ModItems.ITEMS.register(modEventBus);
         ModCreativeTabs.TABS.register(modEventBus);
+
+        modEventBus.addListener(ModEntities::registerAttributes);
 
         LOGGER.info("Loading Deadly Monsters NeoForge port for Minecraft 26.2");
     }
