4a0a5363b5559ad7910de027c45c2e250c9a822c
HansOffice <94523696+HansOffice@users.noreply.github.com>
2026-09-05 23:56:55 +0800

    Add NeoForge mod entrypoint

---
+++ src/main/java/com/dmonsters/DeadlyMonsters.java
@@ -0,0 +1,24 @@
+package com.dmonsters;
+
+import com.dmonsters.registry.ModBlocks;
+import com.dmonsters.registry.ModCreativeTabs;
+import com.dmonsters.registry.ModItems;
+import com.mojang.logging.LogUtils;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+import org.slf4j.Logger;
+
+@Mod(DeadlyMonsters.MOD_ID)
+public final class DeadlyMonsters {
+    public static final String MOD_ID = "dmonsters";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public DeadlyMonsters(IEventBus modEventBus, ModContainer modContainer) {
+        ModBlocks.BLOCKS.register(modEventBus);
+        ModItems.ITEMS.register(modEventBus);
+        ModCreativeTabs.TABS.register(modEventBus);
+
+        LOGGER.info("Loading Deadly Monsters NeoForge port for Minecraft 26.2");
+    }
+}
