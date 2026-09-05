0be4cd98dc1dfec2ced6dd19ae002cca24afccec
HansOffice <94523696+HansOffice@users.noreply.github.com>
2026-09-06 00:49:18 +0800

    Add Freezer client render state

---
+++ src/main/java/com/dmonsters/client/FreezerRenderState.java
@@ -0,0 +1,8 @@
+package com.dmonsters.client;
+
+import net.minecraft.client.renderer.entity.state.LivingEntityRenderState;
+
+public final class FreezerRenderState extends LivingEntityRenderState {
+    public boolean aggressive;
+    public float attackTime;
+}
